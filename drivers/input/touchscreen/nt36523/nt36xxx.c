4ba38107ab2d66768d7e2ae9fd39db6eebf5ace7
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-03 10:30:24 +0800

    nt36xxx: Add wakelock when performing nvt_switch_mode_work
    * this is a temporary fix to make dt2w functional when entering AOD mode, this is noop once device enters deep sleep, ideal workaround for dt2w for ambient pulse
    
    Change-Id: I9023619b0d6a028e2d5bafe98bb7581692670060
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- drivers/input/touchscreen/nt36523/nt36xxx.c
+++ drivers/input/touchscreen/nt36523/nt36xxx.c
@@ -55,6 +55,8 @@
 #include "../xiaomi/xiaomi_touch.h"
 #endif
 
+struct wakeup_source *doubletap_wake_lock;
+
 #if NVT_TOUCH_ESD_PROTECT
 static struct delayed_work nvt_esd_check_work;
 static struct workqueue_struct *nvt_esd_check_wq;
@@ -1969,6 +1971,7 @@ out:
 
 static void nvt_switch_mode_work(struct work_struct *work)
 {
+    __pm_stay_awake(doubletap_wake_lock);
 	NVT_LOG("%s double click wakeup", ts->db_wakeup ? "ENABLE" : "DISABLE");
 	if (ts->ic_state <= NVT_IC_SUSPEND_OUT && ts->ic_state != NVT_IC_INIT ) {
 		ts->gesture_command_delayed = ts->db_wakeup;
@@ -1977,6 +1980,7 @@ static void nvt_switch_mode_work(struct work_struct *work)
 	} else  if (ts->ic_state >= NVT_IC_RESUME_IN){
 		dsi_panel_doubleclick_enable(!!ts->db_wakeup);
 	}
+	__pm_relax(doubletap_wake_lock);
 }
 
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
@@ -2683,6 +2687,7 @@ void nvt_enable_doubleclick(void)
 {
 	int ret = 0;
 	uint8_t buf[4] = {0};
+
 	if (ts->db_wakeup) {
 		buf[0] = EVENT_MAP_HOST_CMD;
 		buf[1] = 0x13;
@@ -2714,6 +2719,8 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	struct attribute_group *attrs_p = NULL;
 
 	NVT_LOG("probe start\n");
+
+    doubletap_wake_lock = wakeup_source_register(NULL, "doubletap_wake_lock");
 
 	ts = kzalloc(sizeof(struct nvt_ts_data), GFP_KERNEL);
 	if (ts == NULL) {
